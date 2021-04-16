f29d47e07a15d53ab88265a7a1825b73be58459e
Anna Sokolova <sokolovaskorik@gmail.com>
2021-04-17 00:39:17 +0200

    Added spinner

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Spinner from './components/Spinner';
 import Title from './components/Title';
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
 import Filter from './components/Filter';
+import operationContacts from './redux/contacts/contacts-operations';
 
-const App = () => {
-  return (
-    <div className="container">
-      <Title title="Phonebook" />
-      <ContactForm />
-      <Filter />
-      <Title title="Contacts" />
-      <ContactList />
-    </div>
-  );
-};
+class App extends Component {
+  state = {};
 
-export default App;
+  componentDidMount() {
+    this.props.fetchAllContacts();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Title title="Phonebook" />
+        <ContactForm />
+        <Filter />
+        <Title title="Contacts" />
+        <ContactList />
+        {this.props.isloadingContacts && <Spinner />}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isloadingContacts: state.contacts.loading,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchAllContacts: () => dispatch(operationContacts.fetchContacts()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
