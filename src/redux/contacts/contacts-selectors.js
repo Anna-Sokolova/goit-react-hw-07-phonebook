0821d0f727cb6da19c35fe141b116c2b2841e5dc
Anna Sokolova <sokolovaskorik@gmail.com>
2021-04-17 03:46:20 +0200

    Added memoizee selector

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,19 +1,22 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 const getLoading = state => state.contacts.loading;
 
 const getAllContacts = state => state.contacts.items;
 
 const getValueFilter = state => state.contacts.filter;
 
-const getFilteredByName = state => {
-  const allContacts = getAllContacts(state);
-  const filter = getValueFilter(state);
+//added memoizee selector
+const getFilteredByName = createSelector(
+  [getAllContacts, getValueFilter],
+  (allContacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
 
-  const normalizedFilter = filter.toLowerCase();
-
-  return allContacts.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter),
-  );
-};
+    return allContacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter),
+    );
+  },
+);
 
 export default {
   getLoading,
